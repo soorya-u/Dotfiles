ec8d1abb39e4275de314f0a26e9860ee89c3a452
Soorya U <soorya.u@think41.com>
2025-09-08 09:12:39 +0530

    chore: renamed files

--- zsh/zsh-highlightings.zsh
+++ zsh/zsh-highlightings.zsh
@@ -6,10 +6,10 @@ ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=blue
 ZSH_HIGHLIGHT_STYLES[alias]=fg=202
 ZSH_HIGHLIGHT_STYLES[builtin]=fg=202
 ZSH_HIGHLIGHT_STYLES[function]=fg=blue
-ZSH_HIGHLIGHT_STYLES[command]=fg=202
+ZSH_HIGHLIGHT_STYLES[command]=fg=214
 ZSH_HIGHLIGHT_STYLES[precommand]=fg=202
 ZSH_HIGHLIGHT_STYLES[commandseparator]=none
-ZSH_HIGHLIGHT_STYLES[hashed-command]=fg=009
+ZSH_HIGHLIGHT_STYLES[hashed-command]=fg=214
 ZSH_HIGHLIGHT_STYLES[path]=fg=white
 ZSH_HIGHLIGHT_STYLES[globbing]=fg=063
 ZSH_HIGHLIGHT_STYLES[history-expansion]=fg=white,underline
